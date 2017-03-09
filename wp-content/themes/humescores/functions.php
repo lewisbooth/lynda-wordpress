81758b434ca3f6216ec290720b48a5a8c1d9b325
Lewis Booth <lewis@lewisbooth.co.uk>
2017-03-09 22:40:01 +0000

    Improved header.php

--- wp-content/themes/humescores/functions.php
+++ wp-content/themes/humescores/functions.php
@@ -64,6 +64,12 @@ function humescores_setup() {
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
+
+	add_theme_support( 'custom-logo', array (
+		'width' => 90,
+		'height' => 90,
+		'flex-width' => true
+	));
 }
 endif;
 add_action( 'after_setup_theme', 'humescores_setup' );
