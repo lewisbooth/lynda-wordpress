88186b1f26c57c140c57c97d0b3ac3a696b486d3
Lewis Booth <lewis@lewisbooth.co.uk>
2017-03-16 22:24:32 +0000

    Rearranged single.php

--- wp-content/themes/humescores/inc/template-tags.php
+++ wp-content/themes/humescores/inc/template-tags.php
@@ -24,43 +24,21 @@ function humescores_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'humescores' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'humescores' ),
+		esc_html_x( 'Written by %s.', 'post author', 'humescores' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	$posted_on = sprintf(
+		esc_html_x( 'Published on %s.', 'post date', 'humescores' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
 
-}
-endif;
 
-if ( ! function_exists( 'humescores_entry_footer' ) ) :
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function humescores_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'humescores' ) );
-		if ( $categories_list && humescores_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'humescores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
+	echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'humescores' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'humescores' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-		}
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
+	if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo ' <span class="comments-link">';
 		/* translators: %s: post title */
 		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'humescores' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
 		echo '</span>';
@@ -72,11 +50,38 @@ function humescores_entry_footer() {
 			esc_html__( 'Edit %s', 'humescores' ),
 			the_title( '<span class="screen-reader-text">"', '"</span>', false )
 		),
-		'<span class="edit-link">',
+		' <span class="edit-link">',
 		'</span>'
 	);
 }
 endif;
+
+if ( ! function_exists( 'humescores_entry_footer' ) ) :
+/**
+ * Prints HTML with meta information for the categories, tags and comments.
+ */
+function humescores_entry_footer() {
+	// Hide tag text for pages.
+	if ( 'post' === get_post_type() ) {
+
+		/* translators: used between list items, there is a space after the comma */
+		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'humescores' ) );
+		if ( $tags_list ) {
+			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'humescores' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+		}
+	}
+}
+endif;
+
+
+function humescores_the_category_list () {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', 'humescores' ) );
+		if ( $categories_list && humescores_categorized_blog() ) {
+			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'humescores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+}
+
 
 /**
  * Returns true if a blog has more than 1 category.
